455a55fbdff9da40b020b52ff5fde07d1a8d269d
Ping <panfilo.glophics@gmail.com>
2021-09-23 21:42:39 +0800

    build

--- src/Kernel.php
+++ src/Kernel.php
@@ -11,21 +11,28 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-     //...
+    protected function configureContainer(ContainerConfigurator $container): void
+    {
+        $container->import('../config/{packages}/*.yaml');
+        $container->import('../config/{packages}/'.$this->environment.'/*.yaml');
 
-     public function getCacheDir()
-     {
-         if ($this->environment === 'prod') {
-             return sys_get_temp_dir();
-         }
-         return parent::getCacheDir();
-     }
- 
-     public function getLogDir()
-     {
-         if ($this->environment === 'prod') {
-             return sys_get_temp_dir();
-         }
-         return parent::getLogDir();
+        if (is_file(\dirname(__DIR__).'/config/services.yaml')) {
+            $container->import('../config/services.yaml');
+            $container->import('../config/{services}_'.$this->environment.'.yaml');
+        } else {
+            $container->import('../config/{services}.php');
+        }
+    }
+
+    protected function configureRoutes(RoutingConfigurator $routes): void
+    {
+        $routes->import('../config/{routes}/'.$this->environment.'/*.yaml');
+        $routes->import('../config/{routes}/*.yaml');
+
+        if (is_file(\dirname(__DIR__).'/config/routes.yaml')) {
+            $routes->import('../config/routes.yaml');
+        } else {
+            $routes->import('../config/{routes}.php');
+        }
     }
 }
